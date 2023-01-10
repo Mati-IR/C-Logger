08b27dde32a1921351d8323f9c660921e90a171e
Mati-IR <mpabich4@gmail.com>
2023-01-10 23:04:30 +0100

    Fixed compile.bat

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
-gcc library.c -o library.o -c
 gcc timestamp.c -o timestamp.o -c
+gcc library.c -o library.o -c
 gcc main.c -o main.o -c
-gcc -o myprog main.o library.o
+gcc -o myprog main.o library.o timestamp.o
 
 rm *.o
